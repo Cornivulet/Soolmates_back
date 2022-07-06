8886c80f76487fea5c1488de2239976b6212d437
Cornivulet <22870013+Cornivulet@users.noreply.github.com>
2022-07-06 17:35:20 +0200

    attempt to fix deployment (debug=true)

--- Soolmates_back/settings.py
+++ Soolmates_back/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-upkq_=z8xzyv*38uo$61%m%3d-=j8xdby*#w^y&xskil*zdn0w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['soolmates.herokuapp.com', 'localhost']
 

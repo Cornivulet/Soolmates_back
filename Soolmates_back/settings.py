61b417d71a69ad48aaf60dcef0f3c9c88525a0b6
Cornivulet <22870013+Cornivulet@users.noreply.github.com>
2022-07-06 17:03:39 +0200

    attempt to fix media deployment bug

--- Soolmates_back/settings.py
+++ Soolmates_back/settings.py
@@ -175,7 +175,7 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Configuration part for media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
